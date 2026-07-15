745cf1759bc0ae65c419e8fd3c8e85fb56ca7bd9
kyhns7 <kyhns7@outlook.com>
2026-07-15 15:42:23 +0800

    refactor(system): move exception handling into system configuration

--- apps/server/gnilc-bootstrap/src/main/java/com/gnilc/bootstrap/AuthApplication.java
+++ apps/server/gnilc-bootstrap/src/main/java/com/gnilc/bootstrap/AuthApplication.java
@@ -1,12 +1,9 @@
 package com.gnilc.bootstrap;
 
-import com.gnilc.common.exception.RestExceptionHandlingConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.gnilc.bootstrap")
-@Import(RestExceptionHandlingConfiguration.class)
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
